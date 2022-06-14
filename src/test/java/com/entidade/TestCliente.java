a9b87f93b872ec2b1eb2815b499e5ce05d376716
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-06-14 11:57:10 -0300

    Melhorando a qualidade.

--- src/test/java/com/entidade/TestCliente.java
+++ src/test/java/com/entidade/TestCliente.java
@@ -1,6 +1,5 @@
 package com.entidade;
 
-import com.entidade.Cliente;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
