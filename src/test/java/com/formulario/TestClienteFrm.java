b66150fa3817051d6eac35d0e96b728b8ed04e6a
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2022-06-16 23:10:21 -0300

    Melhorando a qualidade.

--- src/test/java/com/formulario/TestClienteFrm.java
+++ src/test/java/com/formulario/TestClienteFrm.java
@@ -4,33 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-public class TestClienteFrm {
+class TestClienteFrm {
 
     /**
      * Testa o construtor sem argumentos do cliente.
      */
     @Test
-    public void testCliente() {
+    void testCliente() {
         ClienteFrm instancia = new ClienteFrm();
         assertTrue((instancia.getClienteId()==0) && "".equals(instancia.getNome()) && "".equals(instancia.getCpf()));
     }
 
     @Test
-    public void testClienteIdInt() {
+    void testClienteIdInt() {
         ClienteFrm instancia = new ClienteFrm();
         instancia.setClienteId(1);
         assertTrue((instancia.getClienteId()==1) && "".equals(instancia.getNome()) && "".equals(instancia.getCpf()));
     }
     
     @Test
-    public void testClienteMensage() {
+    void testClienteMensage() {
         ClienteFrm instancia = new ClienteFrm();
         instancia.setMensagem("mensagem");
         assertTrue("mensagem".equals(instancia.getMensagem()));
     }
 
     @Test
-    public void testParaString() {
+    void testParaString() {
         ClienteFrm instancia = new ClienteFrm();
         String esperado = "clienteId:0 - Nome : - CPF :";
         assertEquals(esperado, instancia.toString());
