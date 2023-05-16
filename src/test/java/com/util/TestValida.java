707baa5016a9b031aa10fbe0a3e7065c694b9f27
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-05-16 16:07:05 -0300

    Aumento cobertura.

--- src/test/java/com/util/TestValida.java
+++ src/test/java/com/util/TestValida.java
@@ -41,11 +41,12 @@ class TestValida {
      * Utiliza testes parametrizados.
      */
     @ParameterizedTest
-    @ValueSource(strings = {"123456",       //CPF sem os onze dígitos
+    @ValueSource(strings = {"11111111111",  //CPF todos os dígitos iguais
+                            "123456",       //CPF sem os onze dígitos
                             "12345678908",  //CPF inválido com o primeiro dígito verificador incorreto.
                             "12345678901",  //CPF inválido com o segundo dígito verificador incorreto.
                             "0065XAB22050", //CPF com problema de conversão
-                            ""}             //CPF Vazio
+                            ""}             //CPF Vazio                            
                             )
     void testCPFInvalido(String cpf) {
                   
